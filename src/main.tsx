d3fb53f3ef8d3c8216bdcefc25be18adc24bda70
Denis Kalkopf <deniskalkopf@gmail.com>
2024-06-13 23:18:45 +0500

    add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,48 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from './pages/ErrorPage/ErrorPage.tsx';
+import UserPage from './pages/UserPage/UserPage.tsx';
+import AllUsersPage from './pages/AllUsersPage/AllUsersPage.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: '<RequireAuth/>',
+    children: [
+      {
+        path: '/',
+        element: <AllUsersPage />,
+      },
+      {
+        path: '/users/:id',
+        element: <UserPage />,
+      },
+    ],
+  },
+  {
+    path: '/auth',
+    element: '<AuthLayout />',
+    children: [
+      {
+        path: 'login',
+        element: '<Login />',
+      },
+      {
+        path: 'register',
+        element: '<Register />',
+      },
+    ],
+  },
+  {
+    path: '*',
+    element: <ErrorPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
