01a7a3303f020f9e55e7c0e72a07853b01aef0a3
Denis Kalkopf <deniskalkopf@gmail.com>
2024-06-14 18:03:29 +0500

    add validation

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -1,7 +1,8 @@
-import { FormEvent, useEffect } from 'react';
+import { FormEvent, useEffect, useState } from 'react';
 import styles from './RegisterPage.module.css';
 import { register } from '../../helpers/API';
 import { useNavigate } from 'react-router-dom';
+import { emailValidation } from '../../helpers/formValidation';
 
 export type RegisterForm = {
   email: {
@@ -13,6 +14,14 @@ export type RegisterForm = {
 };
 
 export function RegisterPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailDirty, setEmailDirty] = useState(false);
+  const [passwordDirty, setPasswordDirty] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [formValid, setFormValid] = useState(false);
+
   const navigate = useNavigate();
   const token = localStorage.getItem('token');
 
@@ -22,14 +31,63 @@ export function RegisterPage() {
     }
   }, [token, navigate]);
 
+  useEffect(() => {
+    if (emailError || passwordError) {
+      setFormValid(false);
+    } else {
+      setFormValid(true);
+    }
+  }, [emailError, passwordError]);
+
+  const emailHandler = (e) => {
+    setEmail(e.target.value);
+    console.log(!emailValidation(e.target.value));
+    if (!emailValidation(e.target.value)) {
+      setEmailError(true);
+    } else {
+      setEmailError(false);
+    }
+  };
+
+  const passwordHandler = (e) => {
+    setPassword(e.target.value);
+    if (e.target.value.length < 3) {
+      setPasswordError(true);
+    } else {
+      setPasswordError(false);
+    }
+  };
+
+  const blurHandler = (e) => {
+    switch (e.target.name) {
+      case 'email':
+        setEmailDirty(true);
+        setEmailError(true);
+        break;
+      case 'password':
+        setPasswordDirty(true);
+        setPasswordError(true);
+        break;
+    }
+  };
+
   const submit = async (e: FormEvent) => {
     e.preventDefault();
     const target = e.target as typeof e.target & RegisterForm;
     const { email, password } = target;
-    const res = await register({ email: email.value, password: password.value });
-    if (res.token) {
-      localStorage.setItem('token', res.token);
-      navigate('/');
+
+    if (emailValidation(email.value)) {
+      setEmailError(false);
+
+      const res = await register({ email: email.value, password: password.value });
+      if (res.token) {
+        localStorage.setItem('token', res.token);
+        navigate('/');
+      }
+    } else {
+      console.log(emailError);
+      setEmailError(true);
+      return;
     }
   };
 
@@ -39,21 +97,41 @@ export function RegisterPage() {
         <h2>Регистрация</h2>
         <div className={styles.field}>
           <label htmlFor='name'>Имя</label>
-          <input id='name' name='name' type='text' placeholder='Ваше имя' required />
+          <input id='name' name='name' type='text' placeholder='Ваше имя' />
         </div>
         <div className={styles.field}>
           <label htmlFor='email'>Электронная почта</label>
-          <input id='email' name='email' type='email' placeholder='example@mail.ru' required />
+          <input
+            value={email}
+            onChange={(e) => emailHandler(e)}
+            onBlur={(e) => blurHandler(e)}
+            id='email'
+            name='email'
+            type='email'
+            placeholder='example@mail.ru'
+          />
+          {emailDirty && emailError && <small className={styles.error}>Ошибка</small>}
         </div>
         <div className={styles.field}>
           <label htmlFor='password'>Пароль</label>
-          <input id='password' name='password' type='text' placeholder='Введите пароль' required />
+          <input
+            value={password}
+            onChange={(e) => passwordHandler(e)}
+            onBlur={(e) => blurHandler(e)}
+            id='password'
+            name='password'
+            type='password'
+            placeholder='Введите пароль'
+          />
+          {passwordDirty && passwordError && <small className={styles.error}>Ошибка</small>}
         </div>
         <div className={styles.field}>
           <label htmlFor='confirm'>Подтвердите пароль</label>
-          <input id='confirm' name='confirm' type='text' placeholder='Подтвердите пароль' required />
+          <input id='confirm' name='confirm' type='text' placeholder='Подтвердите пароль' />
         </div>
-        <button className={styles.button}>Зарегистрироваться</button>
+        <button disabled={!formValid} className={styles.button} type='submit'>
+          Зарегистрироваться
+        </button>
       </form>
     </main>
   );
